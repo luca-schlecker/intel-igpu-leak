5143491d9f86d932e4613bbb3efff9ef63204a9e
Luca Schlecker <luca.schlecker@hotmail.com>
2021-03-15 09:36:36 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Intel iGPU Memory Leak
+
+This is a sample project demonstrating the memory leak reported [here](https://www.reddit.com/r/intel/comments/m46fe7/some_intel_igpus_opengl_window_resizing_leads_to/).
---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
+void draw() {
+  glClearColor(1, 0, 0, 1);
+  glClear(GL_COLOR_BUFFER_BIT);
+}
+
+int main(int argc, char** argv) {
+  glfwInit();
+
+  auto win = glfwCreateWindow(1280, 720, "title", nullptr, nullptr);
+
+  glfwMakeContextCurrent(win);
+
+  gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+  glfwSetWindowSizeCallback(win, [](GLFWwindow* window, int width, int height) {
+    draw();
+    glfwSwapBuffers(window);
+  });
+
+  while (!glfwWindowShouldClose(win)) {
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(win);
+  glfwTerminate();
+  return 0;
+}
